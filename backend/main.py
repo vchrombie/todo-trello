a1fe7961fff4090983729791627c668442701eb1
Yamini L <yl9822@nyu.edu>
2023-12-16 02:12:46 -0500

    fix: Tested and fixed errors
    Signed-off-by: Yamini L <yl9822@nyu.edu>

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,28 @@
 import os
 
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
 from pytrello2 import TrelloClient
 
 # Constants for todo-trello
-BOARD = "personal"
-LISTS = ["urgent", "important", "sometime"]
-LIST = "doing"
+# BOARD = "personal"
+# LISTS = ["urgent", "important", "sometime"]
+# LIST = "doing"
 
 # Environment variables
 API_KEY = os.environ.get("TRELLO_API_KEY")
 TOKEN = os.environ.get("TRELLO_TOKEN")
+
+print(TOKEN)
 
 
 app = FastAPI()
 
 
 def get_trello_client(api_key: str, token: str) -> TrelloClient:
-    return TrelloClient(api_key, token)
+    return TrelloClient(token, api_key)
+
 
 client = get_trello_client(API_KEY, TOKEN)
 
@@ -27,15 +30,16 @@ client = get_trello_client(API_KEY, TOKEN)
 @app.get("/")
 async def root():
     # Retrieve all boards
-    boards = client.board.get_all_boards()
+    # boards = client.board.get_all_boards()
 
     # Check if the desired board exists
+    '''
     board_id = None
     for board in boards:
         if board.name == BOARD:
             board_id = board.id
             break
-
+    '''
     # # Create the board if it doesn't exist
     # if board_id is None:
     #     board_id = client.board.create_board(BOARD)
@@ -46,53 +50,39 @@ async def root():
     #     client.board.create_list(board_id, LIST)
 
     # Redirect to '/tasks' endpoint
-    return RedirectResponse(url='/tasks')
+    return RedirectResponse(url="/tasks")
 
 
 @app.get("/tasks")
 async def get_all_tasks():
     # Fetch all tasks from the board
-    tasks = client.card.get_all_cards(BOARD)
-    return {
-        "message": "Tasks retrieved successfully.",
-        "tasks": tasks
-    }
+    tasks = client.list.get_cards_on_list("657d0986c4e8466b450d9d50")
+    return {"message": "Tasks retrieved successfully.", "tasks": tasks}
 
 
 @app.get("/tasks/{task_id}")
-async def get_task(task_id: int):
+async def get_task(task_id: str):
     # Fetch an existing task
     task = client.card.get_card(task_id)
-    return {
-        "message": f"Task {task_id} retrieved successfully.",
-        "task": task
-    }
+    return {"message": f"Task {task_id} retrieved successfully", "tasks": task}
 
 
-@app.post("/tasks/{task_id}")
-async def create_task(task_id: int, task: str = Body(...)):
+@app.post("/tasks/{list_id}")
+async def create_task(list_id: str, name: str, desc: str):
     # Create a new task
-    task = Card(name=task, desc=task, idList=LIST)
-    task_id = client.card.create_card(task, BOARD, LIST)
-    return {
-        "message": f"Task {task_id} created successfully."
-    }
+    task_id = client.card.create_card(idList=list_id, name=name, desc=desc)
+    return {"message": f"Task {task_id} created successfully."}
 
 
-@app.put("/tasks/{task_id}")
-async def update_task(task_id: int, task: str = Body(...)):
+@app.put("/tasks/{card_id}")
+async def update_task(card_id: str, list_id: str, desc: str):
     # Update an existing task
-    task = Card(name=task, desc=task, idList=LIST)
-    client.card.update_card(task_id, task)
-    return {
-        "message": f"Task {task_id} updated successfully."
-    }
+    client.card.update_card(card_id, list_id, desc)
+    return {"message": f"Task {card_id} updated successfully."}
 
 
 @app.delete("/tasks/{task_id}")
-async def delete_task(task_id: int):
+async def delete_task(task_id: str):
     # Delete an existing task
     client.card.delete_card(task_id)
-    return {
-        "message": f"Task {task_id} deleted successfully."
-    }
+    return {"message": f"Task {task_id} deleted successfully."}
